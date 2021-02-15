d5822b89be269a708e8a57914f61e75be116f695
julionsales <69657986+julionsales@users.noreply.github.com>
2021-02-15 11:19:28 -0300

    Scripts para configuração de proxy

---
+++ Configurar Proxy.bat
@@ -0,0 +1,84 @@
+﻿:: Hide Command and Set Scope
+@echo off
+setlocal EnableExtensions
+
+:: Customize Window
+title Configurando Proxy
+
+:: Menu Options
+:: Specify as many as you want, but they must be sequential from 1 with no gaps
+:: Step 1. List the Application Names
+set "App[1]=SMSPLB"
+set "App[2]=REDE_EXECUTIVA"
+
+
+:: Display the Menu
+set "Message= Configuracoa e Correcao de Proxy de Rede"
+:Menu
+cls
+echo.%Message%
+echo.
+echo.  Proxy
+echo.
+set "x=0"
+:MenuLoop
+set /a "x+=1"
+if defined App[%x%] (
+    call echo   %x%. %%App[%x%]%%
+    goto MenuLoop
+)
+echo.
+
+:: Prompt User for Choice
+:Prompt
+set "Input="
+set /p "Input=Selecione seu proxy:"
+
+:: Validate Input [Remove Special Characters]
+if not defined Input goto Prompt
+set "Input=%Input:"=%"
+set "Input=%Input:^=%"
+set "Input=%Input:<=%"
+set "Input=%Input:>=%"
+set "Input=%Input:&=%"
+set "Input=%Input:|=%"
+set "Input=%Input:(=%"
+set "Input=%Input:)=%"
+:: Equals are not allowed in variable names
+set "Input=%Input:^==%"
+call :Validate %Input%
+
+:: Process Input
+call :Process %Input%
+goto End
+
+
+:Validate
+set "Next=%2"
+if not defined App[%1] (
+    set "Message=Invalid Input: %1"
+    goto Menu
+)
+if defined Next shift & goto Validate
+goto :eof
+
+
+:Process
+set "Next=%2"
+call set "App=%%App[%1]%%"
+echo executando script de proxy
+
+if "%App%" EQU "SMSPLB" Powershell.exe -executionpolicy Unrestricted -File  \\smsubatsi002\comp\scripts\proxysmsplb.ps1 | echo Executando configuracao do proxy SMSPLB aguarde...
+
+if "%App%" EQU "REDE_EXECUTIVA" Powershell.exe -executionpolicy Unrestricted -File  \\smsubatsi002\comp\scripts\proxyRedeExecutiva.ps1 | echo Executando configuracao do proxy REDE_EXECUTIVA aguarde...
+
+:: Prevent the command from being processed twice if listed twice.
+set "App[%1]="
+if defined Next shift & goto Process
+goto :eof
+
+
+:End
+endlocal
+echo Pressione uma tecla para sair. 
+pause >nul
---
+++ scripts/proxyRedeExecutiva.bat
@@ -0,0 +1,6 @@
+﻿$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections'
+$data = (Get-ItemProperty -Path $key -Name DefaultConnectionSettings).DefaultConnectionSettings
+$data[4] = 12
+$data[8] = 05
+Set-ItemProperty -Path $key -Name DefaultConnectionSettings -Value $data
+set-itemproperty -path "hkcu:Software\Microsoft\Windows\CurrentVersion\Internet Settings" -name AutoConfigURL -value "http://pac.prodam/proxy/redeexecutiva.pac" -type string
